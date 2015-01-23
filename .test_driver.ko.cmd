c820bf14f2bb45c72fd9014446b711e64a7f8c3f
yige-hu <yige@cs.utexas.edu>
2015-01-23 14:17:22 -0600

    rm hidden files

--- .test_driver.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/yige/system/gpu/driver_hack/test_driver.ko := ld -r -m elf_x86_64 -T /usr/src/linux-headers-3.13.0-44-generic/scripts/module-common.lds --build-id  -o /home/yige/system/gpu/driver_hack/test_driver.ko /home/yige/system/gpu/driver_hack/test_driver.o /home/yige/system/gpu/driver_hack/test_driver.mod.o
--- .tmp_versions/test_driver.mod
+++
@@ -1,2 +0,0 @@
-/home/yige/system/gpu/driver_hack/test_driver.ko
-/home/yige/system/gpu/driver_hack/test_driver.o
